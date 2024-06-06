2e3223d896cf2eb81f8d268de04961e47d776e20
Ethan Poorbaugh <joshua.poorbaugh@moser-it.com>
2024-06-06 13:17:33 -0400

    Uploaded even more files (wow theres a lot of files)

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# streamlit-client-timezone
-Streamlit custom component for getting a client's timezone
+# Streamlit Client Timezone
+Streamlit custom component to get the client's timezone as a tzinfo object. 
+
+## Usage
+```python
+timezone = client_timezone(key="client-timezone")
+
+time_loaded = datetime.now(tz=timezone)
+```
---
+++ client_timezone/frontend/src/MyComponent.tsx
@@ -0,0 +1,33 @@
+import {
+  Streamlit,
+  StreamlitComponentBase,
+  withStreamlitConnection,
+} from "streamlit-component-lib"
+import React, { ReactNode } from "react"
+
+interface State {
+  timeZone: string
+}
+
+/**
+ * This is a React-based component template. The `render()` function is called
+ * automatically when your component should be re-rendered.
+ */
+class MyComponent extends StreamlitComponentBase<State> {
+  public state = { timeZone : "N/A"}
+
+  public render = (): ReactNode => {
+    
+    return (
+      <div style={{display : "none"}}>{Streamlit.setComponentValue(Intl.DateTimeFormat().resolvedOptions().timeZone)}</div>
+    )
+  }
+
+}
+
+// "withStreamlitConnection" is a wrapper function. It bootstraps the
+// connection between your component and the Streamlit app, and handles
+// passing arguments from Python -> Component.
+//
+// You don't need to edit withStreamlitConnection (but you're welcome to!).
+export default withStreamlitConnection(MyComponent)
